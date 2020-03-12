27c1974788a1c4f7587123ba746c28a51e08ccf1
scb6 <scb6@rice.edu>
2020-03-12 15:03:04 -0500

    moving set to later so creds are blasted and adding do_it

--- accounts/accounts-build-ami.sh
+++ accounts/accounts-build-ami.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -xe
-
 SHA=`cat accounts-sha/sha`
 echo "SHA: $SHA"
 
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET
 export AWS_ACCESS_KEY_ID=$AWS_KEY
 
+set -xe 
+
 cd /accounts-deployment/scripts
-./build_image --region us-east-2 --verbose --sha ${SHA}
+./build_image --region us-east-2 --verbose --do_it --sha ${SHA}
