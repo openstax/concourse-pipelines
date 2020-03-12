ec5782efe1aab36c50aeaf90c9c2972c753cac18
scb6 <scb6@rice.edu>
2020-03-12 14:56:25 -0500

    adding aws cred exports

--- accounts/accounts-build-ami.sh
+++ accounts/accounts-build-ami.sh
@@ -3,5 +3,8 @@
 SHA=`cat accounts-sha/sha`
 echo "SHA: $SHA"
 
+export AWS_SECRET_ACCESS_KEY=((AWS_SECRET))
+export AWS_ACCESS_KEY_ID=((AWS_KEY))
+
 cd /accounts-deployment/scripts
 ./build_image --region us-east-2 --verbose --sha ${SHA}
