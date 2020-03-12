9145184c054e4be6db88d19f1ad0aae4be1d4a2a
scb6 <scb6@rice.edu>
2020-03-12 14:59:49 -0500

    fixing vars

--- accounts/accounts-build-ami.sh
+++ accounts/accounts-build-ami.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -xe
+
 SHA=`cat accounts-sha/sha`
 echo "SHA: $SHA"
 
-export AWS_SECRET_ACCESS_KEY=((AWS_SECRET))
-export AWS_ACCESS_KEY_ID=((AWS_KEY))
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET
+export AWS_ACCESS_KEY_ID=$AWS_KEY
 
 cd /accounts-deployment/scripts
 ./build_image --region us-east-2 --verbose --sha ${SHA}
