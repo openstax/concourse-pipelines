e50e48dc28618a22142cb96d0159aa4e73341637
scb6 <scb6@rice.edu>
2020-03-17 17:14:00 -0500

    testing

--- accounts/build-ami/accounts-build-ami.sh
+++ accounts/build-ami/accounts-build-ami.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+pwd
 SHA=`cat accounts-git/.git/short_ref`
 echo "SHA: $SHA"
 
@@ -10,10 +10,11 @@ set -xe
 
 cd /accounts-deployment/scripts
 ./build_image --region us-east-2 --verbose --do_it --sha ${SHA} 2>&1 | tee /tmp/build.out
+grep "AMI:" /tmp/build.out | awk '{print $4}' /tmp/build.out
 
-cd
+cd -
 pwd
 mkdir accounts-ami
-grep "AMI:" /tmp/build.out | awk '{print $4}' /tmp/build.out >> accounts-ami/ami
+grep "AMI:" /tmp/build.out | awk '{print $4}' >> accounts-ami/ami
 
 cat accounts-ami/ami
